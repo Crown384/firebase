b9e619380d8062a6eadc12063a40a45ed2d3cf5a
Crown384 <86677683+Crown384@users.noreply.github.com>
2025-02-26 14:02:34 +0000

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path= require("path");
 
 module.exports = {
-    mode: "genesis",
+    mode: "development",
     entry: "./src/index.js",
     output: {
         path: path.resolve(__dirname, "dist"),
